46655a863ed461a165a0ee89cf07a16c96e923bb
Chris <ckenney008@gmail.com>
2018-01-13 22:52:55 -0500

    make it better

--- bot.js
+++ bot.js
@@ -4,11 +4,12 @@
  const client =  new Discord.Client();
 
 
+
  
 
 client.login('NDAwNzQ5NzY5OTc2MzE1OTA0.DTmadA.T1mEKhOi24uv3M1y-OiOzM_bxyI')
 
- 
+ const prefix = "!"
 
 client.on('message', (message) => {
      if(message.content == "ping") {
@@ -16,26 +17,31 @@ client.on('message', (message) => {
      }
  });
 
- client.on('message', (message) => {
-     if(message.content == '!help') {
-         message.channel.sendMessage('the commands are nothing right now, sorry')
-        
-        }
- });
+
+
 
 
 client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.find('name', 'member-log');
+    const channel = member.guild.channels.find('name', 'Me Sons');
     if (!channel) return;
     channel.send(`Welcome to the server, ${member}`);
   });
+  
 
-client.on('message', member => {
-        if(message.content == '!crole') {
-           var role = member.guild.roles.find('name', 'Me Sons')
-            member.addRole(role)
-        }
+
+client.on('message', message => {
+    if(message.content == prefix + "help") {
+        message.author.sendMessage("There are no commands right now.")
+    }   
 });
+
+client.on('message', message => { 
+    if(message.content == prefix + "betarole") {
+        let role = message.guild.roles.find("name", "Beta Testers");
+        message.guik
+    }
+});
+
 
 
 
