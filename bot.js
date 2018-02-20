e7bdeb6143f8be9f5fa812606254ae5a75d6f9a9
Chris <ckenney008@gmail.com>
2018-02-20 14:26:33 -0500

    first change

--- bot.js
+++ bot.js
@@ -1,48 +1,81 @@
-
-
  const Discord = require("discord.js");
  const client =  new Discord.Client();
 
 
 
- 
 
-client.login(process.env.BOT_TOKEN)
+let queue = {};
 
- const prefix = "!"
 
-client.on('message', (message) => {
-     if(message.content == "ping") {
-        message.channel.sendMessage('pong')
-     }
- });
+let prefix = "!";
+
+
+client.login("NDE0NTk2NDIxMjEyMTc2Mzk0.DWpqhw.Ope1nHrmP6H2GfC12suqL1uam9w");
 
 
 
-
-
-client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.find('name', 'Me Sons');
-    if (!channel) return;
-    channel.send(`Welcome to the server, ${member}`);
+client.on("ready", () => {
+    client.user.setActivity('Owning ' + client.guilds.size + ' servers!!!');
   });
-  
+
+
+   client.on('message', message => {
+    if(message.content == prefix + "listemojis") {
+        const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
+        message.channel.send(emojiList);
+      }
+});
+
+    client.on('message', message => {
+        if(message.content == prefix + 'kick') {
+            let member = message.mentions.members.first();
+            ownerRole = message.member.roles.find('name', 'Admins');
+            modRole = message.member.roles.find('name', 'Moderators');
+                if(!ownerRole && !modRole) {
+                   return message.channel.sendMessage('You don\'t have permissions to kick people');
+                }
+                if(!member) {
+                    return message.reply('Please enter a valid member');
+                }
+                if(!member.kickable)  {
+                   return message.reply('I cannot kick this user!! he/she has a higher role!!');
+                }
+              
+
+          }
+     });
+
+     client.on('message', message => {
+         if(message.content == prefix + 'help') {
+             message.reply('the commands are !listemojis and kick for now :) ')
+         }
+     });
+
+     client.on('guildMemberAdd', member => {
+        console.log('Member' + member.user.username + 'has join the server');
+        
+        client.user.send('Member ' + member.user.username + ' has join the server');
+        
+        var role = member.guild.roles.find('name', 'Members');
+
+        member.addRole(role);
+     });
+
+     client.on('message', message => {
+         if(message.content == 'ping') {
+             message.reply('Congrats, you found a easter egg :)')
+         }
+     });
 
 
 client.on('message', message => {
-    if(message.content == prefix + "help") {
-        message.channel.sendMessage("There are no commands right now.")
-    }   
-});
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
 
-client.on('message', message => { 
-    if(message.content == prefix + "betarole") {
-        let role = message.guild.roles.find("name", "Beta Testers");
-        message.guik
+    if (message.content.startsWith(prefix + 'ping')) {
+        message.channel.sendMessage('Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
     }
 });
-
-
 
 
 
